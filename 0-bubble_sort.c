bcb1fdfd3d929a817b436f8ca57fa4a81423e4ae
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-06-09 15:32:26 -0700

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+
+/**
+ *
+ *
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int x, y, aux;
+
+	for (x = 0; x < ((int)size - 1); x++)
+	{
+		for (y = 0; y < ((int)size - x - 1); y++)
+		{
+			if (array[y] > array[y + 1])
+			{
+				aux = array[y];
+				array[y] = array[y + 1];
+				array[y + 1] = aux;	
+				print_array(array, size);
+			}
+		}
+	}
+}
