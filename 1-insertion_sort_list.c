faff6453c89b2a928f39741e03859d3bf733a3b0
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-06-13 09:02:16 -0700

    maiN

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,58 @@
+#include "sort.h"
+
+/**
+ *
+ *
+ *
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *aux = *list;
+	listint_t *aux2, *aux3;
+
+	while (aux != NULL)
+	{
+		aux2 = aux->prev;
+		while (aux2 != NULL && aux->n < aux2->n)
+		{
+			aux3= aux2->prev;
+			swap(aux, aux2, list);
+			aux2 = aux3;
+			print_list(*list);
+		}
+	aux = aux->next;
+	}
+}
+
+/**
+ *
+ *
+ *
+ */
+void swap(listint_t *actual, listint_t *previous, listint_t **list)
+{
+	listint_t *Naux, *PPAux;
+
+	Naux = actual->next;
+	PPAux = previous->prev;
+
+	actual->next = previous;
+	previous->next = Naux;
+	actual->prev = previous->prev;
+
+	if (Naux != NULL)
+		Naux->prev = previous;
+
+	/*previous->prev = actual;*/
+	if (PPAux != NULL)
+	{
+		previous->prev->next = actual;
+		previous->prev = actual;
+	}
+	else
+	{
+		*list = actual;
+	}
+	if (actual->prev == NULL)
+		*list = actual;
+}
